8baaf96f11efcdc09d6a566d012cad795c4cf3b8
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-08-27 19:11:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ["snickers", "hundred grand". "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand". "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (chocolateBars, name) {
 chocolateBars = ["name", ...chocolateBars]
