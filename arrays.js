6dd0b560bba992ba1258099b19992c1cc81eae5f
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-08-27 19:10:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand". "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (chocolateBars, name) {
+chocolateBars = ["name", ...chocolateBars]
+return chocolateBars
+}
